c99bc228080caabfac31e39c20c530fd0e86655b
Rafal Bromirski <rafal.bromirski@gmail.com>
2016-07-13 20:07:24 +0200

    cleanup

--- src/messenger/core.clj
+++ src/messenger/core.clj
@@ -1,13 +1,10 @@
 (ns messenger.core
   (:require [ring.adapter.jetty :as ring]
-            [compojure.core :refer :all]
-            [compojure.route :refer [not-found]]
             [messenger.handler :refer [app]]
             [messenger.routes :refer [app-routes]]))
 
-(defn start [port]
-  (ring/run-jetty app {:port port
-                       :join? false}))
+(defn- start [port]
+  (ring/run-jetty app {:port port :join? false}))
 
 (defn -main []
   (let [port (Integer. (or (System/getenv "PORT") "3000"))]
